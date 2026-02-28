0029b693339e5a3d76991a6e772d2accbdb8e6c7
pariswashere <2c0061his@gmail.com>
2026-02-28 12:42:18 +0600

    activity4

---
+++ Module 1/Lesson 2/activity4.py
@@ -0,0 +1,12 @@
+x = input("Enter the first number : ")
+y = input("Enter the second number : ")
+
+print("The first number is : ", x)
+print("The second number is : ", y)
+
+temp = x
+x = y
+y = temp
+
+print("After swapping, x is : ", x)
+print("After swapping, y is : ", y)
