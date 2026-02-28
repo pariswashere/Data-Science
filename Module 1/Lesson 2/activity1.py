28e561be10432a1d33144e1c24ad3cff773613f5
pariswashere <2c0061his@gmail.com>
2026-02-28 12:14:58 +0600

    lesson 2

---
+++ Module 1/Lesson 2/activity1.py
@@ -0,0 +1,17 @@
+x = -54
+decimal = 5.84
+name = "Sumera"
+is_student = True
+
+print("The data type of x is : ", type(x))
+print("The data type of decimal is : ", type(decimal))
+print("The data type of name is : ", type(name))
+print("The data type of is_student is : ", type(is_student))
+
+decimal = int(decimal)
+print("The value of decimal after typecasting : ", decimal)
+print("The data type of decimal is : ", type(decimal))
+
+is_student = str(is_student)
+print("The value of is_student after typecasting : ", is_student)
+print("The data type of is_student is : ", type(is_student))
