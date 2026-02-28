2bd64be52db33b2eefeb0e22d367cd2f8fc0129f
pariswashere <2c0061his@gmail.com>
2026-02-28 12:27:59 +0600

    activity2

---
+++ Module 1/Lesson 2/activity2.py
@@ -0,0 +1,14 @@
+num1 = 20
+num2 = 16
+
+print("The first number : ", num1)
+print("The second number : ", num2)
+print("Addition : ", num1+num2)
+print("Subtraction : ", num1-num2)
+print("Multiplication : ", num1*num2)
+print("Division : ", num1/num2)
+print("FLoor Division : ", num1//num2)
+print("Modulus : ", num1%num2)
+print("Square : ", num1**2)
+print("Equal : ", num1==num2)
+print("Not Equal : ", num1!=num2)
