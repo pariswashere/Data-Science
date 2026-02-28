a6910970bd403bc5faa5f97b79e84f176951c1ff
pariswashere <2c0061his@gmail.com>
2026-02-28 12:36:30 +0600

    activity3

---
+++ Module 1/Lesson 2/activity3.py
@@ -0,0 +1,11 @@
+first_name = "Codingal"
+second_name = "Educations"
+full_name = first_name + second_name
+print(full_name)
+example="Tasmia"*5
+print(example)
+word = "Coding"
+print("The length of the word is :", len(word))
+print("The first letter :", word[0])
+print("The second letter :", word[5])
+print("Slicing :", word[0:3])
