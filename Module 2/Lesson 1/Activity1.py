deca3fda021c13defd0d197c80dc522a9fa99560
pariswashere <2c0061his@gmail.com>
2026-03-14 11:19:21 +0600

    l1a1

---
+++ Module 2/Lesson 1/Activity1.py
@@ -0,0 +1,12 @@
+list1 = ["Pizza",3,2.5,"Fries"]
+print(list1[2])
+#Length of the list
+print (len(list1))
+#Add a new element
+list1.append("Papaya")
+print(list1)
+#Remove an element
+list1.remove("Fries")
+print(list1)
+#Slicing an element
+print(list1[1:4])
