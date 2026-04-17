d38ac3dbb6b7691b7ae7c6676e34ab357382a9f5
pariswashere <2c0061his@gmail.com>
2026-04-17 11:32:10 +0600

    a

---
+++ Module 2/Lesson 5/Activity2.py
@@ -0,0 +1,15 @@
+class Person:
+    def __init__(self, fname, lname):
+        self.fname = fname
+        self.lname = lname
+
+    def printname(self):
+        print(self.fname,self.lname)
+class Student(Person):
+    def __init__(self, fname, lname, year):
+        super().__init__(fname, lname)
+        self.graduationyear = year
+
+x = Student("Joey", "King", 2024)
+x.printname()
+print(x.graduationyear)
