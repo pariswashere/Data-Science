ed1f359bd5bedae2820f06a48a7a24619dbd1d54
pariswashere <2c0061his@gmail.com>
2026-05-22 12:16:14 +0600

    e

---
+++ Module 4/Lesson 1/Activity1.py
@@ -0,0 +1,23 @@
+def fun1(n):
+    return n*(n+1)/2
+print(fun1(4))
+
+#Num of iterations
+
+def fun2(n):
+    sum = 0
+    for i in range(1,n+1):
+        sum += i
+    return sum
+print(fun2(4))
+
+#Num of iterations = 4
+
+def fun3(n):
+    sum = 0
+    for i in range(1,n+1):
+        sum += 1
+    return sum
+print(fun3(4))
+
+#Number of iterations = n*n
