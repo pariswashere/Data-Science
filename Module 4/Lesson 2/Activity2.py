682d264c59fa4c429732e021dfddd15908bc8927
pariswashere <2c0061his@gmail.com>
2026-05-23 11:47:00 +0600

    e

---
+++ Module 4/Lesson 2/Activity2.py
@@ -0,0 +1,12 @@
+def OnTime(n):
+    iteration=0
+    for i in range(1,n+1):
+        iteration+=1
+    print("When 'n is ",n,", iterations = ", iteration)
+
+
+OnTime(10)
+OnTime(20)
+OnTime(40)
+
+print("\n With every 'n', the time taken and iterations will increase.")
