8d06c1d2c30dece72d0db818e03f78c50eca03bf
pariswashere <2c0061his@gmail.com>
2026-05-23 11:40:39 +0600

    r

---
+++ Module 4/Lesson 2/Activity1.py
@@ -0,0 +1,10 @@
+def printnumber(n):
+    iteration=0
+    print("The number entered by the user is : ", n)
+    iteration+=1
+    print("The total iterations done by the code is ", iteration,"\n")
+
+
+printnumber(10)
+printnumber(20)
+print("\n With any 'n', the time taken by our code will not change.")
