527d3c5d8ed3d1bfab192050de679cc72df050dc
pariswashere <2c0061his@gmail.com>
2026-05-23 11:53:48 +0600

    e

---
+++ Module 4/Lesson 2/Activity3.py
@@ -0,0 +1,15 @@
+def ONSquareTime(n):
+    iteration = 0
+    for i in range(0,n):
+        for j in range(0,n):
+            print("*", end = " ")
+            iteration+=1
+        print("")
+    print("\n When n is ",n,"Iterations = ", iteration, "\n")
+
+ONSquareTime(5)
+ONSquareTime(4)
+ONSquareTime(3)
+
+print("\nWith every 'n', The time taken equals to n^2")
+print("0(n^2)")
